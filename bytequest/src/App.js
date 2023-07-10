941eaef31f054660258ee9a403861c085039d8b5
Amine-bc <lm_bouchoucha@esi.dz>
2023-07-10 23:33:28 +0100

    init

---
+++ bytequest/src/App.js
@@ -0,0 +1,14 @@
+import Poster from './Components/Poster.jsx';
+
+function App() {
+  return (
+   <>
+   <Poster />
+   <footer className='footer'>
+      <p>© 2023 Byte Quest ELFARI9</p>
+    </footer>
+     </>
+  );
+}
+
+export default App;
---
+++ bytequest/src/Components/Poster.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../index.css'; // Import your CSS file if defined externally
+
+const Poster = () => {
+  return (
+    <div className="container"> {/* Apply the container CSS class or inline styles */}
+      <img className="image" src="../public/Poster.png"  alt ="Poster"/> {}
+      <p className="description"> 
+      Discovering the story of behind the invention is a shared pleasure between all engineering student, and computer science history hides many of these tales, in Byte Quest you will be exploring them !
+      </p> {}
+    </div>
+  );
+};
+
+export default Poster;
