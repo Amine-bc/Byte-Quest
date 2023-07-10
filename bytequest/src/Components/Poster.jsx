acd8650cbfa6775d6d1c79e30123e62cb3fc86d1
Arabet mohamed Ilyes <lm_arabet@esi.dz>
2023-07-11 00:56:42 +0200

    Update Poster.jsx

--- bytequest/src/Components/Poster.jsx
+++ bytequest/src/Components/Poster.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import '../index.css'; // Import your CSS file if defined externally
+import imagePoster from "../../public/Poster.png";
 
 const Poster = () => {
   return (
     <div className="container"> {/* Apply the container CSS class or inline styles */}
-      <img className="image" src="../../public/Poster.png"  alt ="Poster"/> {}
+      <img className="image" src={imagePoster}  alt ="Poster"/> {}
       <p className="description"> 
       Discovering the story of behind the invention is a shared pleasure between all engineering student, and computer science history hides many of these tales, in Byte Quest you will be exploring them !
       </p> {}
