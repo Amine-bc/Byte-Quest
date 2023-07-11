f0d44b66f51f9843f6807d73dca23577ccb73414
ilyes <armilyes@gmail.com>
2023-07-11 19:59:11 +0200

    embedding the game

--- bytequest/src/App.js
+++ bytequest/src/App.js
@@ -1,14 +1,7 @@
 import Poster from './Components/Poster.jsx';
 import BlackButton from './Components/Button.jsx';
 import './index.css';
-function App() {
-  return (
-   <>
-   <Poster />import Poster from './Components/Poster.jsx';
-import BlackButton from './Components/Button.jsx';
-import './index.css';
 import { Unity, useUnityContext } from "react-unity-webgl";
-
 function App() {
   const { unityProvider } = useUnityContext({
     loaderUrl: "./WebGLGame/Build/WebGLGame.loader.js",
@@ -16,6 +9,7 @@ function App() {
     frameworkUrl: "./WebGLGame/Build/WebGLGame.framework.js",
     codeUrl: "./WebGLGame/Build/WebGLGame.wasm",
   });
+
   return (
     <>
     <Poster />
@@ -30,12 +24,3 @@ function App() {
 }
 export default App;
 
-   <BlackButton/>
-   <footer className='footer'>
-      <p>© 2023 Byte Quest ELFARI9</p>
-   </footer>
-     </>
-  );
-}
-
-export default App;
