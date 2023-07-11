bbd58168a00d7f74c1efa6ceccc8265187e94dc1
Arabet mohamed Ilyes <lm_arabet@esi.dz>
2023-07-11 21:58:14 +0200

    Update Button.jsx

--- bytequest/src/Components/Button.jsx
+++ bytequest/src/Components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function BlackButton(props) {
   return (
-<button className="black-button" onClick={() => window.location.href = {props.url}>
+  <button className="black-button" onClick={() => window.location.href = props.url}>
   {props.text}
 </button>
   
